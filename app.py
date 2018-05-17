898b3190a9ba6ac5bf833df751b4493a2d019e0d
FalsytaF <falsyta@outlook.com>
2018-05-17 23:02:40 +0800

    2018/05/17 Second commit

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/problems')
+def problem_page():
+    return render_template('problems.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(port=2333, debug=True)
--- problem.py
+++ problem.py
@@ -1,3 +1,2 @@
 class Problem:
-	def __init__(self, pid, decr_file, conf_file,  ):
-		.
+	def __init__(self, pid, decr_file, conf_file):
