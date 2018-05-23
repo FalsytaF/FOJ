759dbb1790134a3ad9e5441d24716a48bdba0363
FalsytaF <falsyta@outlook.com>
2018-05-23 22:05:50 +0800

    2018/05/23 1st commit

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
 from problempage import md2html
+from user import User
+from usermanage import register_user
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost:3306/FOJ'
+
+db = SQLAlchemy(app)
+db.create_all()
+
+bcrypt = Bcrypt(app)
+
 cdir = os.getcwd()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/problems')
 def problem_list():
@@ -22,13 +32,33 @@ def problem_list():
 def problem_page(pid):
     problem_path = os.path.join(cdir, 'static', 'problems', str(pid))
     decr_file = os.path.join(problem_path, 'decr.md')
-    #print(cdir)
     with open(decr_file, 'r') as fp:
         decr_md = fp.read()
     decr_html = md2html(decr_md)
 
     return decr_html
     #return render_template('problem.html')
+
+@app.route('/createuser', methods=['POST'])
+def create_user():
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+    display_name = request.form['display_name']
+    school = request.form['school']
+
+    password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    register_user(username=username, email=email, password=password, display_name=display_name, school=school)
+
+@app.route('/logincheck', methods=['POST'])
+def login_check():
+    username = request.form['username']
+    password = request.form['password']
+
+    real_password_hash = User.query.filter_by(username=username).first()
+    return bcrypt.check_password_hash(real_password_hash, password)
+
 
 @app.route('/login')
 def login_page():
--- usermanage.py
+++ usermanage.py
@@ -1,10 +1,9 @@
-
-def create_user(username, uid, email, password, display_name, school):
-	user_info = UserInfo(username, uid, email, password, display_name, school)
-	assert db.
-	db.hset('users', username, json.dumps(user_info))
+from user import User
+from flask_sqlalchemy import SQLAlchemy
+from app import db
 
 
-
-def register_user():
-	
+def register_user(username, email, password, display_name, school):
+    user = User(username=username, email=email, password=password, display_name=display_name, school=school)
+    db.session.add(user)
+    db.session.commit()
