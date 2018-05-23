c3dddb76706a5be3d7b0ce1e25b8aa4394579677
yhylord <yhylord@gmail.com>
2018-05-23 22:40:54 +0800

    Remove redundant nullables

--- user.py
+++ user.py
@@ -2,8 +2,8 @@ from app import db
 
 class User(db.Model):
     uid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=False, nullable=False)
-    password = db.Column(db.String(500), unique=False, nullable=False)
-    display_name = db.Column(db.String(100), unique=False, nullable=False)
-    school = db.Column(db.String(100), unique=False, nullable=False)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100), unique=False)
+    password = db.Column(db.String(500), unique=False)
+    display_name = db.Column(db.String(100), unique=False)
+
