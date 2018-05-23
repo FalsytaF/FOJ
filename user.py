71ea884be8254a43a9afbf430ed3d57092a3cf07
FalsytaF <falsyta@outlook.com>
2018-05-23 22:14:27 +0800

    2018/05/23 2st commit

--- user.py
+++ user.py
@@ -3,7 +3,7 @@ from app import db
 class User(db.Model):
     uid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(500), unique=True, nullable=False)
-    display_name = db.Column(db.String(100), unique=True, nullable=False)
-    school = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=False, nullable=False)
+    password = db.Column(db.String(500), unique=False, nullable=False)
+    display_name = db.Column(db.String(100), unique=False, nullable=False)
+    school = db.Column(db.String(100), unique=False, nullable=False)
